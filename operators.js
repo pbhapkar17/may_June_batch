bd6f0a6c119993c143f1319722caf77b9efd7485
pbhapkar17 <poonambhapkar17@gmail.com>
2023-08-03 22:26:22 +0530

    operator

--- operators.js
+++ operators.js
@@ -20,7 +20,7 @@ x = x + 1;
 let y = 10
 //y = y - 1;
 //y--;
-console.log('y',--y);
+console.log('y',y--);
 console.log(y);
 
 //assigment operator = , +=,-=,*=,%=
@@ -31,3 +31,54 @@ let v = 60;   //+= addition assigment operator
 //v = v + r;
 v += r;
 console.log('v-->', v);
+
+let p = 90;
+let q =10;
+p -= q //90-10,p=80
+
+//diff betw =, == , ===
+//Comparison Operators
+// ==  : equality operator >> value
+let k = 40;
+let j = 40;
+console.log( k == j );
+
+//=== : equality operator >> value + data type 
+
+let u = 10;
+let w = '10';
+
+console.log( u == w); //only checks value, so true
+console.log( u === w);// v+ dt , so false
+// > greater than
+
+console.log('>',k > j);
+console.log('>',k < j);
+
+console.log( '!=', u != w);
+console.log( '!==', u !== w);
+
+console.log( '>=', j >= k);
+console.log( '<=', j <= k);
+
+//?: ternory operator::: condition ? true : false;
+
+  let d = 300;
+  d > 200 ? console.log('d is greater')   : console.log('d is smaller'); 
+
+let text1 = "A";//65
+let text2 = 65;//66
+let result = text1 == text2;
+console.log(result);
+
+//Logical Operators &&, ||,! 
+
+let r1 = 90;
+let r2 = 50; 
+
+console.log(r1 > r2 && r2 < r1);   // 90>50 && 50<90 ->>> T && T ->>T
+
+console.log( r1> r2 || r2 > r1); //90 > 50 || 50<90 ->>T || T = T
+                                    //T||F->>T
+let z = false;
+console.log('!-->', !z);
