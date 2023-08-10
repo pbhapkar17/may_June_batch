71e1247c5748f354730be280f57e844483178e09
pbhapkar17 <poonambhapkar17@gmail.com>
2023-08-10 22:43:30 +0530

    string methos

--- stringmethods.js
+++ stringmethods.js
@@ -23,8 +23,17 @@ console.log(text.substr(2,9));
 
 let r = 'Pune method replaces  a specified pune value with another pune value in a stringmethod replaces a specified value with another value in a string';
 
-let q = r.replace(/pune/gi,'mumbai');
+let q = r.replace(/pune/,'mumbai');
 console.log(q);
+
+let j = r.replace(/pune/g,'mumbai');
+console.log(j);
+
+let i = r.replace(/pune/i,'mumbai');
+console.log(i);
+
+let k = r.replace(/pune/gi,'mumbai');
+console.log(k);
 
 let p = r.replaceAll('pune','mumbai');
 console.log(p);
@@ -38,3 +47,49 @@ console.log(f_name);
 
 let F_name = FN.concat(' ', LN, ' ', 'Bhapkar');
 console.log(F_name);
+
+//toLowerCase()
+let city = 'MumB KKKKKKai DFFFFF cvdfvfvfd ';
+let city1 = city.toLowerCase();
+console.log('city1 >>>', city1);
+
+//toUpperCase()
+let city2 = city.toUpperCase();
+console.log('city2 >>>', city2);
+
+//trim()
+let name3 = '   poonam   ';
+
+let name4 = name3.trim();
+console.log('name4>>',name4);
+
+//trimStart()
+let name5 = name3.trimStart();
+console.log('name5',name5);
+
+//trimEnd()
+let name6 = name3.trimEnd();
+console.log('name6',name6);
+
+//charAt()
+let text5 = 'poonam patil';
+console.log(text5.charAt(3));
+
+//split() always return array
+
+let dob = '04_04_1992';
+
+let newDob = dob.split('_');
+console.log(newDob);
+console.log(newDob[2]);
+
+let u = 'poonam A Patil';
+let u2 = u.split(" ");
+console.log(u2);
+console.log(u2[0]);
+console.log(u2[2]);
+
+// String Search Methods
+// String indexOf()
+let d ='dog';
+console.log(d.indexOf('g'));
