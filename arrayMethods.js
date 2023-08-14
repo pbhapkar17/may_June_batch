794b1a3ed67b88125cd5bb5647574a38a678a0b9
pbhapkar17 <poonambhapkar17@gmail.com>
2023-08-14 22:25:20 +0530

    num and arr method

---
+++ arrayMethods.js
@@ -0,0 +1,14 @@
+//length poperty
+
+let no = [10,20,90,66,336,6,6,8,6]
+console.log(no.length);
+
+//.toString();
+let stringesNo = no.toString();
+console.log(stringesNo);
+
+//pop()
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fruit2 = fruits.pop();
+console.log(fruits);
+console.log(fruit2);
