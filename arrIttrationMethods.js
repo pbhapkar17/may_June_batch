18e4ffa610a7db09d43534f53b208b807e4c5bc6
pbhapkar17 <poonambhapkar17@gmail.com>
2023-08-18 22:31:16 +0530

    map

--- arrIttrationMethods.js
+++ arrIttrationMethods.js
@@ -16,3 +16,27 @@ console.log('newCars1',newCars1);
 //spread operator (... )The JavaScript spread operator (...) allows us to quickly copy all or part of an existing array or object into another array or object.
 let newCar2 = [...cars];
 console.log('newCar2',newCar2);
+
+
+//function syntax: function funName(){//code block}
+function test() {
+    //code block
+    //map
+    let employee = [
+        { name: 'poonam', age: 30 },
+        { name: 'pooja', age: 30 },
+        { name: 'om', age: 30 },
+        { name: 'jay', age: 30 }
+    ]
+
+    let empolyeeNames = employee.map(employeeData => {
+        return employeeData.name;
+    })
+    // let empolyeeNames =[];
+    //  employee.map(employeeData=>{
+    //     empolyeeNames.push(employeeData.name);
+    //  })
+
+
+    console.log('empolyeeNames', empolyeeNames);
+}
