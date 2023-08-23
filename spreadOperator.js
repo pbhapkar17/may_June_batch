56cade7b17ac1633afab024b893a0a18cd1b4a2e
pbhapkar17 <poonambhapkar17@gmail.com>
2023-08-23 22:40:48 +0530

    this,

--- spreadOperator.js
+++ spreadOperator.js
@@ -34,3 +34,47 @@ console.log(carDetail2);
 let car4 = {...carDetails, ...carDetails3};
 
 console.log(car4);
+
+
+word = "MAHARASABBAHTRA Mumbai";
+originalWord = word.toLowerCase();// maharasabbahtra mumbai
+console.log('originalWord',originalWord);
+finalString ='';
+for(let i = 0 ; i < this.originalWord.length ; i++){
+    const char = originalWord[i];
+    if(!finalString.includes(char)){
+        finalString += char; //x+=4 => x=x+4;  FS=""+m FS=> m ; 2nd: Fs= FS(m) + a=> ma
+    }
+}
+console.log('finalString',finalString);
+
+//In JavaScript, the this keyword refers to an object.
+let carDetails5 = { 
+    carName : "Swift",
+    color: "white",
+    price: 800000,
+    tpermite: true,
+    dis: function (){
+        return this.price;
+    }
+}
+console.log(carDetails5.dis());
+
+function test(){
+    this.test2()
+}
+
+function test2(){
+    console.log('test2....');
+}
+
+console.log(this.test());
+
+
+// The "this" keyword is used in JavaScript to refer to the current object or context in which the code is being executed.
+// Here are a few ways in which the "this" keyword is commonly used in JavaScript:
+// Inside a method of an object - When used within a method of an object, "this" refers to the object itself. It allows accessing the object's properties and methods from within the method.
+// Function invocation - When a function is invoked in a normal function invocation pattern (not as a method of an object), "this" by default refers to the global object (e.g., the window object in a browser or the global object in Node.js).
+// Constructor functions - When used inside a constructor function, "this" refers to the object being created by that constructor. It allows setting properties and invoking methods specific to the newly created object.
+// Callback functions - When a function is used as a callback function, the value of "this" inside the callback function depends on how the function is invoked. It can vary based on the context in which the callback function is called.
+// Note: It's important to understand that the value of "this" is not determined by where the function is defined but rather by how the function is invoked.
