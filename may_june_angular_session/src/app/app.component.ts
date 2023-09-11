cfcd0961a1476f15387e613abae3da0a3713b82d
pbhapkar17 <poonambhapkar34@gmail.com>
2023-09-11 20:36:47 +0530

    string interpolation

--- may_june_angular_session/src/app/app.component.ts
+++ may_june_angular_session/src/app/app.component.ts
@@ -6,19 +6,32 @@ import { Component } from '@angular/core'; //impot section
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent { //class to write code here
-  title = 'may_june_angular_session';
+  title = 'My first Application';
 //let/var/const city = 'pune';
-//any Data type
+//any Data type: city:any 
 //property
  city? : string ; //declaration of property
 
  city2 : string = 'pune'; //declration + def of proprty
 
  city3 = 'mumbai.....';
-
+ name='poonam';
+ no1 = 10;
+ no2= 200;
   constructor(){}    //to inject dependancies
   
 //life cycle hooks 
 
 //functions
+
+//string interpolation: one way data binding technique: .ts to dom/html/template
+
+learningInterpolation(){
+  //return 'patil';
+  return this.city2;
+  
+}
+
+
+
 }
