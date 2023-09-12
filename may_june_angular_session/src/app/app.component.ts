9ae99cf1d8b3d9f7c722b6a89e562f80224437f4
pbhapkar17 <poonambhapkar34@gmail.com>
2023-09-12 20:48:09 +0530

    one db

--- may_june_angular_session/src/app/app.component.ts
+++ may_june_angular_session/src/app/app.component.ts
@@ -18,20 +18,35 @@ export class AppComponent { //class to write code here
  name='poonam';
  no1 = 10;
  no2= 200;
+ no3='88';
+ data1 = 50;
+ cars : string[] = ['Swift','Jaguar', 'Audi'];
+ car = this.cars[1]; //Jaguar
+ student = { name:'Om', age:20 };
+
+ genderMale = false;
+genderM='male';
+genderF='Female';
+genderFemale = false;
   constructor(){}    //to inject dependancies
   
 //life cycle hooks 
 
 //functions
+//Data binding: It is a technique to link data between a component and the DOM. 
+ //1.one way data binding 2.two way data binding
+//1.one way data binding :1.string interpolation 2.property binding 3.event binding
+//string interpolation:It is a one way data binding technique: .ts/model to dom/html/template {{}}
 
-//string interpolation: one way data binding technique: .ts to dom/html/template
-
-learningInterpolation(){
+learningInterpolation(){  //Void: default type of function 
   //return 'patil';
   return this.city2;
   
 }
 
-
+gen(data:any){
+  console.log('data',data.target.value);
+  
+}
 
 }
