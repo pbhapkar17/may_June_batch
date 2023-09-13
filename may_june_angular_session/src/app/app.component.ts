09c9c839b0c084be30b921e002d84b661cbcfb3e
pbhapkar17 <poonambhapkar34@gmail.com>
2023-09-13 20:31:10 +0530

    pb

--- may_june_angular_session/src/app/app.component.ts
+++ may_june_angular_session/src/app/app.component.ts
@@ -7,46 +7,69 @@ import { Component } from '@angular/core'; //impot section
 })
 export class AppComponent { //class to write code here
   title = 'My first Application';
-//let/var/const city = 'pune';
-//any Data type: city:any 
-//property
- city? : string ; //declaration of property
+  //let/var/const city = 'pune';
+  //any Data type: city:any 
+  //property
+  city?: string; //declaration of property
 
- city2 : string = 'pune'; //declration + def of proprty
+  city2: string = 'pune'; //declration + def of proprty
 
- city3 = 'mumbai.....';
- name='poonam';
- no1 = 10;
- no2= 200;
- no3='88';
- data1 = 50;
- cars : string[] = ['Swift','Jaguar', 'Audi'];
- car = this.cars[1]; //Jaguar
- student = { name:'Om', age:20 };
+  city3 = 'mumbai.....';
+  name = 'poonam';
+  no1 = 10;
+  no2 = 200;
+  no3 = '88';
+  data1 = 50;
+  cars: string[] = ['Swift', 'Jaguar', 'Audi'];
+  car = this.cars[1]; //Jaguar
+  student = { name: 'Om', age: 20 };
+  genderM = 'Male';
+  genderF = 'Female';
+  paintingHobby = "Painting";
+  skechingHobby = "Skeching";
+  readingHobby = "Reading";
+  userHobbies : string[]=[];
+  favCountry = "India";
 
- genderMale = false;
-genderM='male';
-genderF='Female';
-genderFemale = false;
-  constructor(){}    //to inject dependancies
-  
-//life cycle hooks 
+  favFruit = 'Apple';
 
-//functions
-//Data binding: It is a technique to link data between a component and the DOM. 
- //1.one way data binding 2.two way data binding
-//1.one way data binding :1.string interpolation 2.property binding 3.event binding
-//string interpolation:It is a one way data binding technique: .ts/model to dom/html/template {{}}
+  constructor() { }    //to inject dependancies
 
-learningInterpolation(){  //Void: default type of function 
-  //return 'patil';
-  return this.city2;
-  
-}
+  //life cycle hooks 
 
-gen(data:any){
-  console.log('data',data.target.value);
-  
-}
+  //functions
+  //Data binding: It is a technique to link data between a component and the DOM. 
+  //1.one way data binding 2.two way data binding
+  //1.one way data binding :1.string interpolation 2.property binding 3.event binding
+  //string interpolation:It is a one way data binding technique: .ts/model to dom/html/template {{}}
 
+  learningInterpolation() {  //Void: default type of function 
+    //return 'patil';
+    return this.city2;
+
+  }
+
+  gen(data?: any) {
+    console.log('data', data?.target.value);
+
+  }
+
+ hobbies(event : any){
+  console.log(event.target.value);
+  this.userHobbies.push(event.target.value);
+  //this keyword use >> js : this key word is getting used inside object to point object's key
+  //ts: this keyword is used to point global properties.
+  //It is also used in functions to fetch value of global properties.
+  console.log(this.userHobbies);
+  console.log(this.favCountry);
+
+  this.favCountry = 'Maletia';
+  console.log(this.favCountry);
+  this.gen();
+ }
+
+ fruit(){
+  console.log(this.favFruit);
+  this.learningInterpolation();
+ }
 }
