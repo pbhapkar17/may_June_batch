f55a19d85486bebbe21c88e0309ee8dbe05a0397
pbhapkar17 <poonambhapkar34@gmail.com>
2023-09-16 20:26:01 +0530

    event binding

--- may_june_angular_session/src/app/app.component.ts
+++ may_june_angular_session/src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core'; //impot section
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent { //class to write code here
-  title = 'My first Application';
+  title = 'My first Application'; //1.
   //let/var/const city = 'pune';
   //any Data type: city:any 
   //property
-  city?: string; //declaration of property
+  city?: string; //declaration of property //2.
 
-  city2: string = 'pune'; //declration + def of proprty
+  city2: string = 'pune'; //declration + def of proprty //3.
 
   city3 = 'mumbai.....';
   name = 'poonam';
@@ -28,11 +28,15 @@ export class AppComponent { //class to write code here
   paintingHobby = "Painting";
   skechingHobby = "Skeching";
   readingHobby = "Reading";
-  userHobbies : string[]=[];
+  userHobbies: string[] = [];
   favCountry = "India";
   favFruit = 'Apple';
-  studentName:any = 'ssssss';
-  empName=''
+  studentName: any = 'ssssss';
+  empName = '';
+  num1 :number = 10;
+  num2 : number = 20;
+  result?:number;
+
   constructor() { }    //to inject dependancies
 
   //life cycle hooks 
@@ -54,28 +58,46 @@ export class AppComponent { //class to write code here
 
   }
 
- hobbies(event : any){
-  console.log(event.target.value);
-  this.userHobbies.push(event.target.value);
-  //this keyword use >> js : this key word is getting used inside object to point object's key
-  //ts: this keyword is used to point global properties.
-  //It is also used in functions to fetch value of global properties.
-  console.log(this.userHobbies);
-  console.log(this.favCountry);
+  hobbies(event: any) {
+    console.log(event.target.value);
+    this.userHobbies.push(event.target.value);
+    //this keyword use >> js : this key word is getting used inside object to point object's key
+    //ts: this keyword is used to point global properties.
+    //It is also used in functions to fetch value of global properties.
+    console.log(this.userHobbies);
+    console.log(this.favCountry);
 
-  this.favCountry = 'Maletia';
-  console.log(this.favCountry);
-  this.gen();
- }
+    this.favCountry = 'Maletia';
+    console.log(this.favCountry);
+    this.gen();
+  }
 
- fruit(){
-  console.log(this.favFruit);
-  this.learningInterpolation();
- }
+  fruit() {
+    console.log(this.favFruit);
+    this.learningInterpolation();
+  }
 
- submit(event:any){
-console.log(event);
+  submit(event: any) {
+    console.log(event);
 
- }
+  }
 
+  addition() {
+    // let number1 = 20;
+    // let number2 = 30;
+    // var res = number1 + number2; //Addition by variable
+    // console.log('addition result',res);
+    // this.result = parseInt(this.num1) + parseInt(this.num2) ; //Addition by proprties
+    this.result = this.num1 + this. num2;
+     console.log('res-->',this.result);
+     
+  }
+  
+
+addition1(jjknjnj:any){
+ this.num1 = parseInt(jjknjnj.target.value) ;
+}
+addition2(event:any){
+  this.num2 = parseInt(event.target.value)  ;
+}
 }
