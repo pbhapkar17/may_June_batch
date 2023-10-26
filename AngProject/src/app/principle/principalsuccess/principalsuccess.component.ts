83dcf515931eaf13fe17428ef942dea78fd5d682
pbhapkar17 <poonambhapkar34@gmail.com>
2023-10-26 20:47:48 +0530

    ngfor

--- AngProject/src/app/principle/principalsuccess/principalsuccess.component.ts
+++ AngProject/src/app/principle/principalsuccess/principalsuccess.component.ts
@@ -8,14 +8,37 @@ import { Component } from '@angular/core';
 export class PrincipalsuccessComponent {
   showAddr: boolean = false;
   city: boolean = false;
+  cars = ['Swift','jaguar','MiniCuper','Audi'];
+  students = [
+    {name:'tushar',age:20},
+    {name:'Pooja',age:23},
+    {name:"om",age:22}
+  ];
 
-  showAddress() {
+  emp = [
+    {
+      name:
+        { firstName: 'poonam', lastName: 'patil' }
+    },
+    {
+      name:
+        { firstName: 'Ravi', lastName: 'patil' }
+    },
+    {
+      name:
+        { firstName: 'Shekhar', lastName: 'patil' }
+    }
+  ]
+  toshowAddress() {
     //this.showAddr = true;
       this.showAddr = !this.showAddr;  //!f --> t, !t ->f
   }
   toShowCity() {
     // this.city = true;
     this.city = !this.city;
+  }
+  toshowCars(){
+
   }
 
 
